a11c372f0867d60a51e48ad10128829c6d7303b6
Hoang Oscar Dao <43139435+oscardao@users.noreply.github.com>
2020-03-27 12:06:38 +0100

    added drawing

--- StealMySheep/Common/src/main/java/assets/entityComponents/Drawing.java
+++ StealMySheep/Common/src/main/java/assets/entityComponents/Drawing.java
@@ -6,6 +6,9 @@
 package assets.entityComponents;
 
 import assets.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import game.GameData;
 
 /**
@@ -14,8 +17,57 @@ import game.GameData;
  */
 public class Drawing implements Component {
 
+    private Sprite sprite;
+    private Texture texture;
+
+    public Drawing(String imagePath) {
+        this.texture = new Texture(Gdx.files.internal(imagePath));
+        this.sprite = new Sprite(this.texture);
+    }
+
+    public void setRotation(float rotation) {
+        this.sprite.setRotation(rotation);
+    }
+
+    public void flipVertical() {
+        this.sprite.flip(false, true);
+    }
+
+    public void setScale(float scaleX, float scaleY) {
+        this.sprite.setScale(scaleX, scaleY);
+    }
+
+    public void setSize(float width, float height) {
+        this.sprite.setSize(width, height);
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+
     @Override
     public void update(Entity entity, GameData gameData) {
+
+        Position position = entity.getComponent(Position.class);
+        if (position != null) {
+            float x = position.getX();
+            float y = position.getY();
+
+            this.sprite.setPosition(x, y);
+        }
+
     }
 
 }
