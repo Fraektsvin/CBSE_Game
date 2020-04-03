3c8ee00c28737e54007f43aaeb941b3b73ccaaa5
Hoang Oscar Dao <43139435+oscardao@users.noreply.github.com>
2020-04-03 12:49:00 +0200

    added installer class and dependencies

---
+++ StealMySheep/Island/src/main/java/group12/stealmysheep/island/InstallérKlassen.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group12.stealmysheep.island;
+
+/**
+ *
+ * @author Antonio
+ */
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.openide.modules.ModuleInstall;
+
+public class InstallérKlassen extends ModuleInstall {
+
+    @Override
+    public void restored() {
+
+        LwjglApplicationConfiguration cfg
+                = new LwjglApplicationConfiguration();
+        cfg.title = "StealMySheep";
+        cfg.width = 600;
+        cfg.height = 500;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
+        new LwjglApplication(new Island(), cfg);
+
+    }
+
+}
--- StealMySheep/Island/src/main/java/group12/stealmysheep/island/Island.java
+++ StealMySheep/Island/src/main/java/group12/stealmysheep/island/Island.java
@@ -15,56 +15,63 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import group12.stealmysheep.Manager.GameInputProcessor;
 import group12.stealmysheep.Manager.Manager;
-import group12.stealmysheep.common.game.Input; 
-public class Island implements ApplicationListener{
-	
-	public static int WIDTH;
-	public static int HEIGHT;
-	private Input inputs;
-	public static OrthographicCamera cam;
-	
-	private Manager gsm;
-	
-        @Override
-	public void create() {
-		
-		WIDTH = Gdx.graphics.getWidth();
-		HEIGHT = Gdx.graphics.getHeight();
-		
-		cam = new OrthographicCamera(WIDTH, HEIGHT);
-		cam.translate(WIDTH / 2, HEIGHT / 2);
-		cam.update();
-		
-		Gdx.input.setInputProcessor(
-			new GameInputProcessor()
-		);
-		
-		gsm = new Manager();
-		
-	}
-	
-        @Override
-	public void render() {
-		
-		// clear screen to black
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.draw();
-		
-		inputs.update();
-		
-	}
-	
-        @Override
-	public void resize(int width, int height) {}
-        @Override
-	public void pause() {}
-        @Override
-	public void resume() {}
-        @Override
-	public void dispose() {}
-	
-}
+import group12.stealmysheep.common.game.Input;
 
+public class Island implements ApplicationListener {
+
+    public static int WIDTH;
+    public static int HEIGHT;
+    private Input inputs;
+    public static OrthographicCamera cam;
+
+    private Manager gsm;
+
+    @Override
+    public void create() {
+
+        WIDTH = Gdx.graphics.getWidth();
+        HEIGHT = Gdx.graphics.getHeight();
+
+        cam = new OrthographicCamera(WIDTH, HEIGHT);
+        cam.translate(WIDTH / 2, HEIGHT / 2);
+        cam.update();
+
+        Gdx.input.setInputProcessor(
+                new GameInputProcessor()
+        );
+
+        gsm = new Manager();
+
+    }
+
+    @Override
+    public void render() {
+
+        // clear screen to black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.draw();
+
+        inputs.update();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+}
