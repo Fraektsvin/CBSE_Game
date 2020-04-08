2b9eac06173d64f276442b415955f012cb90bc46
nibas18 <43138810+nibas18@users.noreply.github.com>
2020-04-08 13:57:41 +0200

    Random movement
    Played a little with the random movement but it's not done at all and it is "out commented".
    
    Co-Authored-By: naimoibrahim75 <naimoibrahim75@users.noreply.github.com>

--- StealMySheep/Sheep/src/main/java/stealmysheep/sheep/SheepControlSystem.java
+++ StealMySheep/Sheep/src/main/java/stealmysheep/sheep/SheepControlSystem.java
@@ -5,10 +5,12 @@
  */
 package stealmysheep.sheep;
 
+import java.util.Random;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
 import stealmysheep.common.assets.Entity;
 import stealmysheep.common.assets.Sheep;
+import stealmysheep.common.assets.entityComponents.Movement;
 import stealmysheep.common.assets.entityComponents.Position;
 import stealmysheep.common.game.GameData;
 import stealmysheep.common.game.World;
@@ -21,16 +23,27 @@ import stealmysheep.common.services.IUpdate;
 @ServiceProviders(value = {
     @ServiceProvider(service = IUpdate.class),})
 public class SheepControlSystem implements IUpdate {
-
+//
+//    private Random random = new Random();
+//    private int processCounter = 0;
+//    
     @Override
     public void update(GameData gameData, World world) {
         for (Entity sheep : world.getEntities(Sheep.class)) {
             Position position = sheep.getComponent(Position.class);
-
+//            Movement movement = sheep.getComponent(Movement.class);
+//
+//              if (processCounter % 10 == 0) {
+//                movement.setLeft(random.nextBoolean());
+//                movement.setRight(random.nextBoolean());
+//            }
+//            movement.setUp(random.nextBoolean());
+//
+//            movement.update(sheep, gameData);
             position.update(sheep, gameData);
-
+//
+//            processCounter++;
         }
-
     }
 
-}
+        }
