c0627e9a227a8d80cf0ed79da11df859ec90dca2
Fraektsvin <antonio_gonzales@live.dk>
2020-04-09 12:54:47 +0200

    ændret taster
    ændret taster

--- StealMySheep/Island/src/main/java/group12/stealmysheep/Manager/GameInputProcessor.java
+++ StealMySheep/Island/src/main/java/group12/stealmysheep/Manager/GameInputProcessor.java
@@ -23,18 +23,27 @@ public class GameInputProcessor extends InputAdapter {
     }
 
     public boolean keyDown(int k) {
-        if (k == Keys.UP) {
-            gameData.getInput().setKeyStatus(Input.UP, true);
+
+        if (k == Keys.W) {
+            inputs.setKeyStatus(Input.UP, true);;
         }
-        if (k == Keys.LEFT) {
-            gameData.getInput().setKeyStatus(Input.LEFT, true);
+        if (k == Keys.A) {
+            inputs.setKeyStatus(inputs.LEFT, true);
         }
-        if (k == Keys.DOWN) {
-            gameData.getInput().setKeyStatus(Input.DOWN, true);
+        if (k == Keys.S) {
+            inputs.setKeyStatus(inputs.DOWN, true);
         }
-        if (k == Keys.RIGHT) {
-            gameData.getInput().setKeyStatus(Input.RIGHT, true);
+        if (k == Keys.D) {
+            inputs.setKeyStatus(inputs.RIGHT, true);
+
         }
+        if (k == Keys.R) {
+            inputs.setKeyStatus(inputs.R, false);
+        }
+        if (k == Buttons.LEFT) {
+            inputs.setKeysStatus(Inputs.MOUSELEFT,false);
+        }
+       
         if (k == Keys.ENTER) {
             gameData.getInput().setKeyStatus(Input.ENTER, true);
         }
@@ -51,18 +60,27 @@ public class GameInputProcessor extends InputAdapter {
     }
 
     public boolean keyUp(int k) {
-        if (k == Keys.UP) {
-            gameData.getInput().setKeyStatus(Input.UP, false);
+
+        if (k == Keys.W) {
+            inputs.setKeyStatus(inputs.UP, false);
         }
-        if (k == Keys.LEFT) {
-            gameData.getInput().setKeyStatus(Input.LEFT, false);
+        if (k == Keys.A) {
+            inputs.setKeyStatus(inputs.LEFT, false);
         }
-        if (k == Keys.DOWN) {
-            gameData.getInput().setKeyStatus(Input.DOWN, false);
+        if (k == Keys.S) {
+            inputs.setKeyStatus(inputs.DOWN, false);
         }
-        if (k == Keys.RIGHT) {
-            gameData.getInput().setKeyStatus(Input.RIGHT, false);
+        if (k == Keys.D) {
+            inputs.setKeyStatus(inputs.RIGHT, false);
+
         }
+        if (k == Keys.R) {
+            inputs.setKeyStatus(inputs.R, false);
+        }
+         if (k == Buttons.LEFT) {
+            inputs.setKeysStatus(Inputs.MOUSELEFT,false);
+        }
+        
         if (k == Keys.ENTER) {
             gameData.getInput().setKeyStatus(Input.ENTER, false);
         }
