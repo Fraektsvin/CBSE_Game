ecb53dec4dd6a651386231f6cb7f163ca7785660
Nadin Fariss <nafar18@student.sdu.dk>
2020-04-11 13:45:23 +0200

    something

--- StealMySheep/Weapon/src/main/java/stealmysheep/weapon/WeaponUpdate.java
+++ StealMySheep/Weapon/src/main/java/stealmysheep/weapon/WeaponUpdate.java
@@ -5,10 +5,19 @@
  */
 package stealmysheep.weapon;
 
+import stealmysheep.common.game.GameData;
+import stealmysheep.common.game.World;
+import stealmysheep.common.services.IUpdate;
+
 /**
  *
  * @author nadinfariss
  */
-public class WeaponUpdate {
+public class WeaponUpdate implements IUpdate {
+
+    @Override
+    public void update(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
