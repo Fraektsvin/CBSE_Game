dbe480684c18ceaaf98577d5c95b3b988227e125
Antonio <32570073+Fraektsvin@users.noreply.github.com>
2020-04-22 17:20:23 +0200

    delete
    Deleting non functional class

--- StealMySheep/Island/src/main/java/group12/stealmysheep/Manager/SpawnManager.java
+++ StealMySheep/Island/src/main/java/group12/stealmysheep/Manager/SpawnManager.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package group12.stealmysheep.island;
+package group12.stealmysheep.Manager;
 
 /**
  *
  * @author Antonio
  */
-public class Spawn {
+public class SpawnManager {
+    
     
 }
