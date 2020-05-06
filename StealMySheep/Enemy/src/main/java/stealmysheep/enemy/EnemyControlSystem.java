2926b69ac341d0e0b8f5eae9837d04970a371f3e
NaimoIbrahim75 <43139535+NaimoIbrahim75@users.noreply.github.com>
2020-05-06 12:38:00 +0200

    Edit
    Add EnemyCreator class as well as produced setTagets for player and sheep. Plus a checkTargetExistence

--- StealMySheep/Enemy/src/main/java/stealmysheep/enemy/EnemyControlSystem.java
+++ StealMySheep/Enemy/src/main/java/stealmysheep/enemy/EnemyControlSystem.java
@@ -5,10 +5,13 @@
  */
 package stealmysheep.enemy;
 
+import java.util.Random;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
 import stealmysheep.common.assets.Enemy;
 import stealmysheep.common.assets.Entity;
+import stealmysheep.common.assets.Player;
+import stealmysheep.common.assets.Sheep;
 import stealmysheep.common.assets.entityComponents.BoxCollider;
 import stealmysheep.common.assets.entityComponents.Movement;
 import stealmysheep.common.assets.entityComponents.Position;
@@ -25,89 +28,63 @@ import stealmysheep.common.services.IUpdate;
 public class EnemyControlSystem implements IUpdate {
 
     private Entity sheep;
+    private EnemyCreator enemy = new EnemyCreator();
+    private Random random;
 
     @Override
     public void update(GameData gameData, World world) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private Entity createThief(GameData gameData) {
-
-        float acceleration = 60;
-        float speed = 30;
-        float x = gameData.getSceneWidth() / 2;
-        float y = gameData.getSceneHeight() / 2;
-        float radians = 3.1415f / 2;
-        float height = 50;
-        float width = 70;
-        boolean targetPlayer = false;
-        boolean targetSheep = true;
-        float targetRadius = 125;
-
-        Entity thief = new Enemy(targetPlayer, targetSheep, targetRadius, "thief.png");
-        thief.addComponent(new Movement(acceleration, speed));
-        thief.addComponent(new Position(x, y, radians));
-        thief.addComponent(new BoxCollider(height, width));
-        return thief;
-    }
-
-    private Entity createRock(GameData gameData) {
-        float acceleration = 60;
-        float speed = 30;
-        float x = gameData.getSceneWidth() / 2;
-        float y = gameData.getSceneHeight() / 2;
-        float radians = 3.1415f / 2;
-        float height = 50;
-        float width = 70;
-        boolean targetPlayer = true;
-        boolean targetSheep = false;
-        float targetRadius = 125;
-
-        Entity rock = new Enemy(targetPlayer, targetSheep, targetRadius, "rock.png");
-        rock.addComponent(new Movement(acceleration, speed));
-        rock.addComponent(new Position(x, y, radians));
-        rock.addComponent(new BoxCollider(height, width));
-        return rock;
-
-    }
-
-    private Entity createArcher(GameData gameData) {
-        float acceleration = 60;
-        float speed = 30;
-        float x = gameData.getSceneWidth() / 2;
-        float y = gameData.getSceneHeight() / 2;
-        float radians = 3.1415f / 2;
-        float height = 50;
-        float width = 70;
-        boolean targetPlayer = true;
-        boolean targetSheep = false;
-        float targetRadius = 125;
-
-        Entity archer = new Enemy(targetPlayer, targetSheep, targetRadius, "archer.png");
-        archer.addComponent(new Movement(acceleration, speed));
-        archer.addComponent(new Position(x, y, radians));
-        archer.addComponent(new BoxCollider(height, width));
-        return archer;
-
-    }
-
     private void moveEnemy() {
         // ryk enemy mod target. Her tjekkes der ikke for target. Det sker i metoden under.
-
+        
     }
 
-    private boolean checkTargetExistence(World world) {
-        // tjekker om target findes og return true når den findes og false når den ikke eksisterer. 
-        return false;
+    private boolean checkTargetExistence(World world, Enemy enemy) {
+        // tjekker om target findes og return true når den findes og false når den ikke eksisterer.
+        Entity target = enemy.getTarget();
+        if(target == null){
+            
+            return false;
+        }
+        Entity entity = world.getEntity(target.getId());
+        if(entity == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    private void setTargetPlayer(World world, Enemy enemy){
+        if (enemy.willTargetPlayer()) {
+            Entity player = world.getEntities(Player.class).get(0);
+            if (player == null) {
+                return;
+            }
 
+            Position playerPosition = player.getComponent(Position.class);
+            Position enemyPosition = enemy.getComponent(Position.class);
+            if ((Math.pow(playerPosition.getX() - enemyPosition.getX(), 2) + Math.pow(playerPosition.getY() - enemyPosition.getY(), 2)) < Math.pow(enemy.getTargetRadius(), 2)) {
+                enemy.setTarget(player);
+            }
+        }
     }
 
-    private void setTarget(World world) {
+    private void setTargetSheep(World world, Enemy enemy) {
 // hvis begge (tjek modul beskrivelser) true første prio lig sheep. 
 // hvis den første er true og den anden er false er første prio player. 
-// første metode der gøres brug af ligeså snart der spawnes. 
+// første metode der gøres brug af ligeså snart der spawnes.
+        if (enemy.willTargetSheep()) {
+            int randomSheep = random.nextInt(world.getEntities(Sheep.class).size());
+            if (randomSheep == 0) {
+                return;
+            }
+            enemy.setTarget(world.getEntities(Sheep.class).get(randomSheep));
+        }
     }
+
     private void attack() {
         
+
     }
 }
---
+++ StealMySheep/Enemy/src/main/java/stealmysheep/enemy/EnemyCreator.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stealmysheep.enemy;
+
+import stealmysheep.common.assets.Enemy;
+import stealmysheep.common.assets.Entity;
+import stealmysheep.common.assets.entityComponents.BoxCollider;
+import stealmysheep.common.assets.entityComponents.Movement;
+import stealmysheep.common.assets.entityComponents.Position;
+import stealmysheep.common.game.GameData;
+
+/**
+ *
+ * @author Naimo
+ */
+public class EnemyCreator {
+    
+    public Entity createThief(GameData gameData) {
+
+        float acceleration = 60;
+        float speed = 30;
+        float x = gameData.getSceneWidth() / 2;
+        float y = gameData.getSceneHeight() / 2;
+        float radians = 3.1415f / 2;
+        float height = 50;
+        float width = 70;
+        boolean targetPlayer = false;
+        boolean targetSheep = true;
+        float targetRadius = 125;
+
+        Entity thief = new Enemy(targetPlayer, targetSheep, targetRadius, "thief.png");
+        thief.addComponent(new Movement(acceleration, speed));
+        thief.addComponent(new Position(x, y, radians));
+        thief.addComponent(new BoxCollider(height, width));
+        return thief;
+    }
+
+    public Entity createRock(GameData gameData) {
+        float acceleration = 60;
+        float speed = 30;
+        float x = gameData.getSceneWidth() / 2;
+        float y = gameData.getSceneHeight() / 2;
+        float radians = 3.1415f / 2;
+        float height = 50;
+        float width = 70;
+        boolean targetPlayer = true;
+        boolean targetSheep = false;
+        float targetRadius = 125;
+
+        Entity rock = new Enemy(targetPlayer, targetSheep, targetRadius, "rock.png");
+        rock.addComponent(new Movement(acceleration, speed));
+        rock.addComponent(new Position(x, y, radians));
+        rock.addComponent(new BoxCollider(height, width));
+        return rock;
+
+    }
+
+    public Entity createArcher(GameData gameData) {
+        float acceleration = 60;
+        float speed = 30;
+        float x = gameData.getSceneWidth() / 2;
+        float y = gameData.getSceneHeight() / 2;
+        float radians = 3.1415f / 2;
+        float height = 50;
+        float width = 70;
+        boolean targetPlayer = true;
+        boolean targetSheep = false;
+        float targetRadius = 125;
+
+        Entity archer = new Enemy(targetPlayer, targetSheep, targetRadius, "archer.png");
+        archer.addComponent(new Movement(acceleration, speed));
+        archer.addComponent(new Position(x, y, radians));
+        archer.addComponent(new BoxCollider(height, width));
+        return archer;
+
+    }
+    
+}
