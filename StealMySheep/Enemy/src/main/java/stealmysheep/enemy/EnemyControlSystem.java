aa2ee549f68c19d88615cd5d166ef905bdc7597f
NaimoIbrahim75 <43139535+NaimoIbrahim75@users.noreply.github.com>
2020-05-07 12:23:52 +0200

    edit
    edits not working

--- StealMySheep/Enemy/src/main/java/stealmysheep/enemy/EnemyControlSystem.java
+++ StealMySheep/Enemy/src/main/java/stealmysheep/enemy/EnemyControlSystem.java
@@ -6,8 +6,11 @@
 package stealmysheep.enemy;
 
 import java.util.Random;
+import org.openide.util.Lookup;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
+import stealmysheep.common.ai.IAI;
+import stealmysheep.common.ai.Node;
 import stealmysheep.common.assets.Enemy;
 import stealmysheep.common.assets.Entity;
 import stealmysheep.common.assets.Player;
@@ -28,15 +31,55 @@ import stealmysheep.common.services.IUpdate;
 public class EnemyControlSystem implements IUpdate {
 
     private Entity sheep;
+    private Entity player;
     private EnemyCreator enemy = new EnemyCreator();
     private Random random;
+    private IAI ai;
+    private final Lookup lookup = Lookup.getDefault();
 
     @Override
     public void update(GameData gameData, World world) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.ai = lookup.lookup(IAI.class);
+        if (ai == null) {
+            return;
+        }
+        for (Entity enemy : world.getEntities(Enemy.class)) {
+            Enemy thief = (Enemy) enemy;
+            Enemy rock = (Enemy) enemy;
+            Enemy archer = (Enemy) enemy;
+            //Sheep currentSheep = (Sheep) sheep;
+            Player currentPlayer = (Player) player;
+            BoxCollider collider = enemy.getComponent(BoxCollider.class);
+            Position position = enemy.getComponent(Position.class);
+            Movement movement = enemy.getComponent(Movement.class);
+
+            if (thief.isMoving() == true) {
+                Node goal = new Node(Sheep currentSheep = (Sheep) sheep.ge.getX(), currentSheep.getY());
+            }
+
+            for (Entity entity : world.getEntities()) {
+                if (entity.hasComponent(BoxCollider.class) && entity.hasComponent(Position.class) && !entity.equals(currentSheep)) {
+                    BoxCollider entityCollider = entity.getComponent(BoxCollider.class);
+                    Position entityPosition = entity.getComponent(Position.class);
+                    if (entityCollider.checkPointCollider(x, y, entityPosition.getX(), entityPosition.getY())) {
+                        currentSheep.setMoving(false);
+                    }
+                }
+            }
+            //System.out.println("Position: x=" + position.getX() + "  y=" + position.getY() + "       Goal: x=" + goal.getX() + "  y=" + goal.getY());
+            this.ai.moveEntity(thief, rock, archer, goal, world, gameData);
+            if (collider.checkPointCollider(goal.getX(), goal.getY(), position.getX(), position.getY())) {
+                System.out.println("Goal reached");
+                currentSheep.setMoving(false);
+
+            }
+
+        }
     }
 
-    private boolean checkTargetExistence(World world, Enemy enemy) {
+}
+
+private boolean checkTargetExistence(World world, Enemy enemy) {
         // tjekker om target findes og return true når den findes og false når den ikke eksisterer.
         Entity target = enemy.getTarget();
         if(target == null){
@@ -48,33 +91,56 @@ public class EnemyControlSystem implements IUpdate {
             return false;
         }else{
             return true;
-        }
+
+}
     }
     private void setTargetPlayer(World world, Enemy enemy){
         if (enemy.willTargetPlayer()) {
-            Entity player = world.getEntities(Player.class).get(0);
-            if (player == null) {
-                return;
-            }
+            Entity player = world.getEntities(Player.class  
 
-            Position playerPosition = player.getComponent(Position.class);
-            Position enemyPosition = enemy.getComponent(Position.class);
-            if ((Math.pow(playerPosition.getX() - enemyPosition.getX(), 2) + Math.pow(playerPosition.getY() - enemyPosition.getY(), 2)) < Math.pow(enemy.getTargetRadius(), 2)) {
-                enemy.setTarget(player);
-            }
-        }
+    ).get(0);
+    if (player
+
+    
+        == null) {
+                return;
     }
+
+    Position playerPosition = player.getComponent(Position.class);
+    Position enemyPosition = enemy.getComponent(Position.class);
+
+    if ((Math.pow (playerPosition.getX
+
+    () - enemyPosition.getX(), 2) + Math.pow(playerPosition.getY() - enemyPosition.getY(), 2)) < Math.pow(enemy.getTargetRadius 
+        (), 2)) {
+                enemy.setTarget(player);
+    }
+}
+
+
+}
 
     private void setTargetSheep(World world, Enemy enemy) {
 // hvis begge (tjek modul beskrivelser) true første prio lig sheep. 
 // hvis den første er true og den anden er false er første prio player. 
 // første metode der gøres brug af ligeså snart der spawnes.
         if (enemy.willTargetSheep()) {
-            int randomSheep = random.nextInt(world.getEntities(Sheep.class).size());
-            if (randomSheep == 0) {
+            int randomSheep = random.nextInt(world.getEntities(Sheep.class  
+
+    ).size());
+    if (randomSheep
+
+    
+        == 0) {
                 return;
-            }
-            enemy.setTarget(world.getEntities(Sheep.class).get(randomSheep));
+    }
+
+    enemy.setTarget (world.getEntities
+
+    (Sheep.class  
+    
+
+).get(randomSheep));
         }
     }
 
