d74f352aa60eea6dfa0799b4ead2caeaa991ac6d
nibas18 <43138810+nibas18@users.noreply.github.com>
2020-05-07 09:33:38 +0200

    bug fixes
    Co-Authored-By: Hoang Oscar Dao <oscardao@users.noreply.github.com>

--- StealMySheep/Sheep/src/main/java/stealmysheep/sheep/SheepControlSystem.java
+++ StealMySheep/Sheep/src/main/java/stealmysheep/sheep/SheepControlSystem.java
@@ -62,10 +62,10 @@ public class SheepControlSystem implements IUpdate {
                     currentSheep.setY(position.getY() + y);
 
                     for (Entity entity : world.getEntities()) {
-                        if (entity.hasComponent(BoxCollider.class) && entity.hasComponent(Position.class) && entity.equals(currentSheep)) {
+                        if (entity.hasComponent(BoxCollider.class) && entity.hasComponent(Position.class) && !entity.equals(currentSheep)) {
                             BoxCollider entityCollider = entity.getComponent(BoxCollider.class);
                             Position entityPosition = entity.getComponent(Position.class);
-                            if (!entityCollider.checkPointCollider(x, y, entityPosition.getX(), entityPosition.getY())) {
+                            if (entityCollider.checkPointCollider(x, y, entityPosition.getX(), entityPosition.getY())) {
                                 currentSheep.setMoving(false);
                             }
                         }
